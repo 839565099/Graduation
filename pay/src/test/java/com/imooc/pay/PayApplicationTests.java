b250bf7d8edc683f9202adf443c7202a645497c9
张翼麒 <839565099@qq.com>
2020-03-20 14:09:51 +0800

    Squashed commit of the following:
    commit 61a1202147528f002c12d9649c2a4cdf2e1b5769
    Author: 张翼麒 <839565099@qq.com>
    Date:   Fri Mar 20 13:45:30 2020 +0800
    
        修改xxx

--- pay/src/test/java/com/imooc/pay/PayApplicationTests.java
+++ pay/src/test/java/com/imooc/pay/PayApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 张翼麒
+ */
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class PayApplicationTests {
